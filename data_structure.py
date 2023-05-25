e8d356f057362939f3e6dbe57d98aca0839c6caa
Zhiying Chen <zhiying.chen@youhualin.com>
2023-05-25 14:04:12 +0800

    update main

--- data_structure.py
+++ data_structure.py
@@ -1,12 +1,15 @@
 from config import CarSetting
 class Car:
-    def __init__(self, id, t0, left_work_dur=CarSetting.init_left_work_dur):
+    def __init__(self, id, t0, left_total_dur=0, left_work_dur=CarSetting.init_left_work_dur):
         self.id = id
         self.earliest_avl_time = t0
         self.left_work_dur = left_work_dur
+        self.left_total_dur = left_total_dur
+        self.init_left_total_dur = left_total_dur
 
         self.upload_dur = CarSetting.upload_dur
         self.unpack_dur = CarSetting.unpack_dur
+        self.prepare_dur = CarSetting.prepare_dur
         self.leave_dur = CarSetting.leave_dur
         self.return_dur = CarSetting.return_dur
         self.min_serve_dur = CarSetting.min_serve_dur
@@ -19,7 +22,10 @@ class Car:
         if self.earliest_avl_time <= curr_t and self.left_work_dur - self.leave_dur - self.return_dur >= self.min_serve_dur:
             return True
         return False
-
+    def is_able_work_v2(self, curr_t):
+        if self.earliest_avl_time <= curr_t and self.left_total_dur - self.leave_dur - self.return_dur - self.upload_dur - self.unpack_dur - self.prepare_dur >= self.min_serve_dur:
+            return True
+        return False
     def get_serve_dur(self):
         return min(self.max_serve_dur, self.left_work_dur - self.leave_dur - self.return_dur)
 
@@ -30,16 +36,36 @@ class Car:
 
         # update earliest_avl_time
         if self.left_work_dur - self.leave_dur - self.return_dur < self.min_serve_dur:
-            self.earliest_avl_time = curr_t + self.rest_dur + self.upload_dur + self.unpack_dur + self.leave_dur + self.return_dur + serve_dur
-            self.schedule[curr_t] = {'serve': self.upload_dur + self.unpack_dur + self.leave_dur + self.return_dur + serve_dur}
-            self.schedule[curr_t + self.upload_dur + self.unpack_dur + self.leave_dur + self.return_dur + serve_dur] = {'rest': self.rest_dur}
+            self.earliest_avl_time = curr_t + self.rest_dur + self.upload_dur + self.unpack_dur + self.leave_dur + self.return_dur + serve_dur +  self.prepare_dur
+            self.schedule[curr_t] = {'serve': self.upload_dur + self.unpack_dur + self.leave_dur + self.return_dur + serve_dur + self.prepare_dur}
+            self.schedule[curr_t + self.upload_dur + self.unpack_dur + self.prepare_dur+ self.leave_dur + self.return_dur + serve_dur] = {'rest': self.rest_dur}
             self.left_work_dur = CarSetting.init_left_work_dur
         else:
-            self.earliest_avl_time = curr_t + self.upload_dur + self.unpack_dur + self.leave_dur + self.return_dur + serve_dur
+            self.earliest_avl_time = curr_t + self.upload_dur + self.unpack_dur + self.prepare_dur + self.leave_dur + self.return_dur + serve_dur
             self.schedule[curr_t] = {
-                'serve': self.upload_dur + self.unpack_dur + self.leave_dur + self.return_dur + serve_dur}
+                'serve': self.upload_dur + self.unpack_dur + self.prepare_dur + self.leave_dur + self.return_dur + serve_dur}
 
     def update_rest_and_earliest_avl_time(self, curr_t):
         self.schedule[curr_t] = {'rest': self.rest_dur}
         self.earliest_avl_time = curr_t + self.rest_dur
         self.left_work_dur = CarSetting.init_left_work_dur
+
+    def update_left_total_dur_and_earliest_avl_time(self, curr_t):
+        # update left_work_dur
+        self.left_total_dur = self.left_total_dur - self.max_serve_dur - self.leave_dur - self.return_dur - self.upload_dur - self.unpack_dur
+
+        # update earliest_avl_time
+        if self.left_total_dur - self.leave_dur - self.return_dur - self.upload_dur - self.unpack_dur - self.prepare_dur < self.max_serve_dur:
+            self.earliest_avl_time = curr_t + self.rest_dur + self.upload_dur + self.unpack_dur + self.prepare_dur+ self.leave_dur + self.return_dur + self.max_serve_dur
+            self.schedule[curr_t] = {'serve': self.upload_dur + self.unpack_dur + self.prepare_dur +self.leave_dur + self.return_dur + self.max_serve_dur}
+            self.schedule[curr_t + self.upload_dur + self.unpack_dur + self.prepare_dur + self.leave_dur + self.return_dur + self.max_serve_dur] = {'rest': self.rest_dur}
+            self.left_total_dur = self.init_left_total_dur
+        else:
+            self.earliest_avl_time = curr_t + self.upload_dur + self.unpack_dur + self.prepare_dur + self.leave_dur + self.return_dur + self.max_serve_dur
+            self.schedule[curr_t] = {
+                'serve': self.upload_dur + self.unpack_dur + self.prepare_dur + self.leave_dur + self.return_dur + self.max_serve_dur}
+
+    def update_rest_and_earliest_avl_time_v2(self, curr_t):
+        self.schedule[curr_t] = {'rest': self.rest_dur}
+        self.earliest_avl_time = curr_t + self.rest_dur
+        self.left_total_dur = self.init_left_total_dur
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import numpy as np
+
 from greedy import run_greedy_algorithm
-from config import CarSetting, planHorizon
+from config import CarSetting, planHorizon, Sign
 from data_structure import Car
 from math import floor, ceil
 import pandas as pd
+import numpy as np
+from time import time
 
 def get_valid_car_num(greedy_car_dict):
     num = 0
@@ -12,15 +16,22 @@ def get_valid_car_num(greedy_car_dict):
             num += 1
     return num
 
+def judge_car_avl(t, car_dict):
+    for c_id, car in car_dict.items():
+        if car.is_able_work_v2(t):
+            return True
+    return False
+
 def get_avl_car(t, car_dict):
     best_c = None
     min_left_dur = float('inf')
     for c_id, car in car_dict.items():
-        if car.is_able_work(t):
-            if car.left_work_dur <= min_left_dur:
+        if car.is_able_work_v2(t):
+            if car.left_total_dur <= min_left_dur:
                 best_c = c_id
-                min_left_dur = car.left_work_dur
+                min_left_dur = car.left_total_dur
     return best_c
+
 
 def output(car_dict):
     record_dict = {}
@@ -28,77 +39,111 @@ def output(car_dict):
         record = {k: '' for k in range(planHorizon.start_time, planHorizon.end_time + 1)}
         for t, event in car.schedule.items():
             if 'serve' in event.keys():
-                serve_dur = event['serve'] - car.upload_dur - car.leave_dur - car.return_dur - car.unpack_dur
+                serve_dur = event['serve'] - car.upload_dur - car.leave_dur - car.return_dur - car.unpack_dur - car.prepare_dur
                 curr_t = t
 
                 for i in range(car.upload_dur):
-                    record[curr_t] = '↑'
+                    record[curr_t] = Sign.upload
                     curr_t += 1
+
                 for i in range(car.leave_dur):
-                    record[curr_t] = '→'
+                    record[curr_t] = Sign.leave
                     curr_t += 1
 
                 for i in range(serve_dur):
-                    record[curr_t] = '口'
+                    record[curr_t] = Sign.serve
                     curr_t += 1
 
                 for i in range(car.return_dur):
-                    record[curr_t] = '←'
+                    record[curr_t] = Sign.back
                     curr_t += 1
                 for i in range(car.unpack_dur):
-                    record[curr_t] = '↓'
+                    record[curr_t] = Sign.unpack
+                    curr_t += 1
+                for i in range(car.prepare_dur):
+                    record[curr_t] = Sign.prepare
                     curr_t += 1
             elif 'rest' in event.keys():
-                curr_t = t
-                for i in range(car.rest_dur):
-                    record[curr_t] = 'zZ'
-                    curr_t += 1
+                if t == 0:
+                    curr_t = t
+                    for i in range(car.rest_dur):
+                        record[curr_t] = Sign.fix
+                        curr_t += 1
+                else:
+                    curr_t = t - car.prepare_dur
+                    for i in range(-car.prepare_dur, car.rest_dur):
+                        record[curr_t] = Sign.fix
+                        curr_t += 1
 
         record_dict[c_id] = record
 
     out_df = pd.DataFrame(record_dict, dtype=object)
-    out_df = out_df.T
+    out_df = out_df.reset_index(drop=True)
+    out_df.replace('', Sign.spare, inplace=True)
     out_df.to_csv('output.csv')
 
 if __name__ == '__main__':
-    greedy_car_dict = run_greedy_algorithm()
 
-    one_round_dur = CarSetting.upload_dur + CarSetting.leave_dur + CarSetting.max_serve_dur + CarSetting.return_dur + CarSetting.unpack_dur  # 跑一趟需要的总时长
+    st = time()
+    one_round_dur = CarSetting.upload_dur + CarSetting.leave_dur + CarSetting.max_serve_dur + CarSetting.return_dur + CarSetting.unpack_dur + CarSetting.prepare_dur # 跑一趟需要的总时长
     one_round_work_dur = CarSetting.leave_dur + CarSetting.max_serve_dur + CarSetting.return_dur   # 跑一趟需要的工作时长
     continuous_round_num = floor(CarSetting.init_left_work_dur / one_round_work_dur)            # 保养之前可以连续跑几趟车
 
-    valid_car_num = get_valid_car_num(greedy_car_dict)     # 休息时间内，至少需要多少辆车正常工作
+
     interval = ceil(one_round_dur / CarSetting.max_serve_dur) * CarSetting.max_serve_dur  # 同一辆车的最小发车间隔
-    gradient = CarSetting.rest_dur / interval   # 休息的时间可以跑几趟车
-    rest_slope = gradient * one_round_work_dur  # 每两辆车之间需要错开的剩余工作时间
+    rest_round_num = ceil(CarSetting.rest_dur / interval)
 
-    car_num = ceil(valid_car_num * ((one_round_dur * continuous_round_num + CarSetting.rest_dur)/ (one_round_dur * continuous_round_num)))  # 至少需要几辆车
 
-    init_points = [planHorizon.start_time + i * CarSetting.max_serve_dur for i in range(car_num)]
+    rest_slope = rest_round_num * one_round_dur   # 每两辆车之间需要错开的剩余总时间
+    init_total_dur = one_round_dur * continuous_round_num
+
+    last_left_work_dur = 0
+    t = planHorizon.start_time
+
+
 
     car_dict = {}
-    id = 0
-    last_left_work_dur = rest_slope
-    t = planHorizon.start_time
+
+    id = 1
+    new_car = Car(id, t, left_total_dur=last_left_work_dur)  # 初始化一辆新车
+    new_car.init_left_total_dur = init_total_dur
+    if last_left_work_dur == 0:
+        new_car.update_rest_and_earliest_avl_time_v2(t)
+
+    car_dict[new_car.id] = new_car
+    id += 1
+
+
     while t <= planHorizon.end_time:
-
-        if t in init_points:
-            new_car = Car(id, t, left_work_dur=last_left_work_dur)  # 初始化一辆新车
-            id += 1
-
-            new_car.update_left_work_dur_and_earliest_avl_time(CarSetting.max_serve_dur, t)
-
-            car_dict[new_car.id] = new_car
-            if (last_left_work_dur + rest_slope) % CarSetting.init_left_work_dur == 0:
-                last_left_work_dur = CarSetting.init_left_work_dur
-            else:
-                last_left_work_dur = (last_left_work_dur + rest_slope) % CarSetting.init_left_work_dur
-
-        else:
+        avl = judge_car_avl(t, car_dict)
+        if avl:
             c_id = get_avl_car(t, car_dict)
             car = car_dict[c_id]
-            car.update_left_work_dur_and_earliest_avl_time(CarSetting.max_serve_dur, t)
 
-        t += CarSetting.max_serve_dur
+            car.update_left_total_dur_and_earliest_avl_time(t)
+            t += CarSetting.max_serve_dur
+        else:
+            car = car_dict[id - 1]
+            last_action_key = max(car.schedule.keys())
+            last_action = car.schedule[last_action_key]
+            if 'serve' in last_action:
+                last_left_total_dur = car.left_total_dur + last_action['serve']
+            else:
+                last_left_total_dur = 0
 
+            if last_left_total_dur + rest_slope > init_total_dur:
+                left_total_dur = (last_left_total_dur + rest_slope) % init_total_dur
+            else:
+                left_total_dur = last_left_total_dur + rest_slope
+            new_car = Car(id, t, left_total_dur=left_total_dur)  # 初始化一辆新车
+            new_car.init_left_total_dur = init_total_dur
+            new_car.update_left_total_dur_and_earliest_avl_time(t)
+            car_dict[new_car.id] = new_car
+            id += 1
+
+            t += CarSetting.max_serve_dur
+
+    print(time() - st)
     output(car_dict)
+
+
