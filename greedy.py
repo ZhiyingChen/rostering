b9d9acd18555f9b8ae233c01b7f876df1a5977b0
Zhiying Chen <zhiying.chen@youhualin.com>
2023-05-26 18:00:29 +0800

    add read config

--- greedy.py
+++ greedy.py
@@ -1,6 +1,7 @@
 from data_structure import Car
-from config import planHorizon
+from config import planHorizon, Sign, CarSetting, read_config
 import pandas as pd
+from time import time
 
 def judge_car_avl(t, car_dict):
     for c_id, car in car_dict.items():
@@ -23,7 +24,7 @@ def run_greedy_algorithm():
     t = planHorizon.start_time  # plan horizon start time
     car_dict = {}
 
-    id = 0
+    id = 1
     while t <= planHorizon.end_time:
         if t == planHorizon.start_time:
             new_car = Car(id, t)
@@ -58,46 +59,63 @@ def run_greedy_algorithm():
 
     return car_dict
 
+
 def greedy_output(car_dict):
     record_dict = {}
     for c_id, car in car_dict.items():
         record = {k: '' for k in range(planHorizon.start_time, planHorizon.end_time + 1)}
         for t, event in car.schedule.items():
             if 'serve' in event.keys():
-                serve_dur = event['serve'] - car.upload_dur - car.leave_dur - car.return_dur - car.unpack_dur
+                serve_dur = event['serve'] - car.upload_dur - car.leave_dur - car.return_dur - car.unpack_dur - car.prepare_dur
                 curr_t = t
 
                 for i in range(car.upload_dur):
-                    record[curr_t] = '↑'
+                    record[curr_t] = Sign.upload
                     curr_t += 1
+
                 for i in range(car.leave_dur):
-                    record[curr_t] = '→'
+                    record[curr_t] = Sign.leave
                     curr_t += 1
 
                 for i in range(serve_dur):
-                    record[curr_t] = '口'
+                    record[curr_t] = Sign.serve
                     curr_t += 1
 
                 for i in range(car.return_dur):
-                    record[curr_t] = '←'
+                    record[curr_t] = Sign.back
                     curr_t += 1
                 for i in range(car.unpack_dur):
-                    record[curr_t] = '↓'
+                    record[curr_t] = Sign.unpack
+                    curr_t += 1
+                for i in range(car.prepare_dur):
+                    record[curr_t] = Sign.prepare
                     curr_t += 1
             elif 'rest' in event.keys():
-                curr_t = t
-                for i in range(car.rest_dur):
-                    record[curr_t] = 'zZ'
-                    curr_t += 1
+                if t == 0:
+                    curr_t = t
+                    for i in range(car.rest_dur):
+                        record[curr_t] = Sign.fix
+                        curr_t += 1
+                else:
+                    curr_t = t - car.prepare_dur
+                    for i in range(-car.prepare_dur, car.rest_dur):
+                        record[curr_t] = Sign.fix
+                        curr_t += 1
 
         record_dict[c_id] = record
 
     out_df = pd.DataFrame(record_dict, dtype=object)
-    out_df = out_df.T
+    out_df.replace('', Sign.spare, inplace=True)
+    out_df = out_df.loc[planHorizon.start_time:planHorizon.end_time]
     out_df.to_csv('greedy_output.csv')
 
-car_dict = run_greedy_algorithm()
-greedy_output(car_dict)
+if __name__ == '__main__':
+    read_config()
+
+    st = time()
+    car_dict = run_greedy_algorithm()
+    print(time() - st)
+    greedy_output(car_dict)
 
 
 
