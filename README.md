a467f8af9ce8f3f4ea915111f6e31f3eb2831de3
Zhiying Chen <nicolec1999@foxmail.com>
2025-07-05 22:08:22 +0800

    feat: update pages“

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ emoji: 🚀
 colorFrom: red
 colorTo: red
 sdk: docker
-app_port: 8501
 tags:
 - streamlit
 pinned: false
@@ -12,9 +11,38 @@ short_description: Streamlit template space
 license: mit
 ---
 
-# Welcome to Streamlit!
+# Rostering
+    
+ **Problem**
+ 
+This is a project to solve the following problem.
+<img src="https://github.com/ZhiyingChen/Rostering/blob/master/image/Question.png" style="width:900px; height:400px">
 
-Edit `/src/streamlit_app.py` to customize this app to your heart's desire. :heart:
+The service area must have at least 1 vehicle serving locally every hour, and each vehicle must return after 6 hours of work in the service area. Each vehicle needs to be repaired for 40h after 80h of cumulative operation during working hours. Q: How many vehicles are needed at least in a month (30 days)? And draw the Gantt chart of the tasks of these cars.
+(All parameters can be changed).
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+**Result Example**
+
+This is a gantt plot.
+<img src="https://github.com/ZhiyingChen/Rostering/blob/master/image/example.png" style="width:1000px; height:300px">
+
+If we change two parameters, for instance, the service area must have at least 2 vehicles serving locally every hour, and each vehicle needs to be repaired for 40h after 200h of cumulative operation during working hours. Other parameters remain unchanged.
+We get the following gantt plot of results.
+<img src="https://github.com/ZhiyingChen/Rostering/blob/master/image/example2.png" style="width:1000px; height:300px">
+
+**Environment Deployment**
+
+ Install Python Executor (version >= 3.7.0), Anaconda IDE is recommanded
+
+
+The required packages are listed in requirements.txt. you can install them using:
+
+    pip install -r requirements.txt
+ 
+ **Run**
+
+To run project:
+
+    1. put your input data in data.csv, you can adjust parameters.
+    2. execute python main.py
+    3. execute python gantt.py to draw gantt pics of of your result.
---
+++ pages/2_⚙️_执行算法.py
@@ -0,0 +1,125 @@
+import streamlit as st
+import pandas as pd
+import time
+from web import function, gantt
+from singleRoster import config as cg
+from singleRoster.solver import Solver
+
+st.title("⚙️ 汽车轮转与维修调度算法")
+
+st.markdown(
+    """
+    请在“执行算法”页面将示例输入文件改成你需要的数据。
+    """
+)
+
+# 输入接口文档
+# 输入接口文档
+st.header("📥 输入接口文档")
+
+with st.expander("📥 输入文件说明：全局参数.csv"):
+    df_global_params = pd.DataFrame([
+        ["参数名称", "str", "各类参数的名称"],
+        ["参数值", "double", "参数值"]
+    ], columns=["字段名称", "类型", "描述"])
+    st.table(df_global_params)
+
+    st.markdown("""
+        **参数枚举说明：**
+        以下是各个参数的定义和含义：
+    
+        - **t_pack**：装货时间，车辆在装货点需要花费的时间。
+        - **t_unpack**：卸货时间，车辆在卸货点需要花费的时间。
+        - **t_pre**：备货时间，车辆在维修点进行备货的时间，这部分时间可以计入维修时间。
+        - **t_go**：去程时间，车辆从起点出发到达服务区域的时间。
+        - **t_back**：返程时间，车辆从服务区域返回起点的时间。
+        - **t_serve**：单次服务时间（小时），车辆在服务区域内一次连续提供服务的时间。
+        - **t_rest**：维修时间（小时），车辆进行维修所需的时间。
+        - **t_work**：每辆车的最大累计工作时间（小时），车辆在进行服务和维修之前可以累计的最大工作时间。
+        - **t0 / t1**：起始时间 / 结束时间，服务时间的开始和结束的具体时间点。
+        - **t_gap**：错峰时间间隔（小时），在排班时需要考虑的时间间隔，以避免车辆在同一时间段集中出发或返回。
+    """)
+
+st.header("📥 输出接口文档")
+with st.expander("📥 输出文件说明：车辆和执行结果.csv"):
+    st.markdown(
+        """
+        每一行代表时间（小时），每一列代表每辆车，内容值含义如下
+        - 装货： 0
+        - 出发： 1
+        - 服务： 2
+        - 返回： 3
+        - 卸货： 4
+        - 备货： 5
+        - 维修： 6
+        - 空闲： 7
+        """
+    )
+
+st.markdown("---")
+# 示例数据展示
+st.header("📄 示例输入数据（可编辑）")
+
+
+@st.cache_data
+def load_csv(file):
+    return pd.read_csv(file)
+
+
+# 加载示例数据
+global_df = load_csv("data/data.csv")
+
+# 可编辑的 DataFrame
+with st.expander("📝 编辑全局参数"):
+    edited_global_df = st.data_editor(global_df, num_rows="dynamic")
+    # 下载按钮
+    st.download_button(
+        label="📥 下载编辑后的 全局参数.csv",
+        data=edited_global_df.to_csv(index=False).encode('utf-8'),
+        file_name="全局参数.csv",
+        mime="text/csv"
+    )
+
+# 显示运行按钮
+if st.button("🚀 运行算法"):
+    with st.spinner("算法运行中，请稍候..."):
+        try:
+            st_time = time.time()
+
+            env = Solver(stTime=cg.planHorizon.start_time, edTime=cg.planHorizon.end_time,
+                         serveNum=cg.serveInfo.serveNum,
+                         upload_dur=cg.CarSetting.upload_dur, unpack_dur=cg.CarSetting.unpack_dur,
+                         prepare_dur=cg.CarSetting.prepare_dur,
+                         leave_dur=cg.CarSetting.leave_dur, return_dur=cg.CarSetting.leave_dur,
+                         serve_dur=cg.CarSetting.serve_dur,
+                         rest_dur=cg.CarSetting.rest_dur, full_dur=cg.CarSetting.full_dur,
+                         load_from_file=False,
+                         config_df=global_df
+                         )
+
+            env.read_config()
+            env.generate_car_schedule()
+            env.generate_car_distribution()
+            print("The validity of this schedule is {}".format(env.check_validity4dist()))
+
+            result_df = env.output_df()
+            st.success("✅ 算法运行完成！耗时{}秒".format(round(time.time() - st_time)))
+        except Exception as e:
+            st.error(f"❌ 算法运行出错：{e}")
+
+        st.markdown("---")
+        st.header("📊 输出结果: 至少需要{}辆车".format(len(result_df.columns)))
+
+        with st.expander("📄 车辆和执行结果"):
+            st.dataframe(result_df)
+            st.download_button(
+                label=f"📥 下载 车辆和执行结果.csv",
+                data=result_df.to_csv(index=False),
+                file_name="车辆和执行结果.csv",
+                mime="text/csv"
+            )
+        with st.spinner("正在绘制甘特图..."):
+            gantt.plot_gantt_bar(result_df)
+
+
+function.render_footer()
